16525ad4720f46014a3137a0e0d6e4dea4cc70c9
MentalMentos <rkorolev026@gmail.com>
2024-11-30 23:59:30 +0300

    logger + configs + postgres rewrite

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"github.com/MentalMentos/techTaskMetr.git/internal/config"
-	"github.com/MentalMentos/techTaskMetr.git/internal/controller"
+	repo "github.com/MentalMentos/techTaskMetr.git/internal/repository"
+	"github.com/MentalMentos/techTaskMetr.git/pkg/helpers"
+	zaplogger "github.com/MentalMentos/techTaskMetr.git/pkg/logger/zap"
 	"github.com/gin-gonic/gin"
 	_ "github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 	_ "github.com/go-playground/validator/v10"
 	"net/http"
 )
@@ -18,20 +19,40 @@ func main() {
 		ctx.JSON(http.StatusOK, "welcome home")
 	})
 
+	// logger init
+	myLogger := zaplogger.New()
+
+	// init env config
+	if err := config.DatabaseConnection(); err != nil {
+		myLogger.Fatal("[ ENV ]", "failed to connect to database")
+	}
+
+	// init config
+	config := config.New(myLogger)
+
 	db := config.DatabaseConnection()
-	validate := validator.New()
 
-	db.Table("tags").AutoMigrate(&model.User{})
+	// init database
+	postgres, err := repo.NewRepository()
+	if err != nil {
+		myLogger.Fatal(helpers.PgPrefix, helpers.PgConnectFailed)
+	}
 
-	// Repository
-	Repository := repository.NewRepo(db)
+	// Keep Alive Postgres
+	go db.KeepAlivePostgres(postgres, myLogger)
 
-	// Service
-	service := service.New(Repository, validate)
+	// init new app
+	myApp := app.New(postgres, myLogger, config)
 
-	// Controller
-	Controller := controller.NewAuthController(service)
+	// migrations
+	if err = db.MigrationUp(config, myLogger); err != nil {
+		myLogger.Fatal(helpers.PgPrefix, err.Error())
+	}
 
-	// TODO: routes
+	// start server
+	err = myApp.Run(myLogger, postgres)
+	if err != nil {
+		myLogger.Fatal(helpers.AppPrefix, err.Error())
+	}
 
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,10 +2,41 @@ package config
 
 import (
 	"fmt"
+	"github.com/MentalMentos/techTaskMetr.git/pkg/logger"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"os"
+	"sync"
 )
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+}
+
+var (
+	config Config
+	once   sync.Once
+)
+
+// New returns Config struct with env variables
+func New(logger logger.Logger) *Config {
+	once.Do(func() {
+		config = Config{
+			Host:     os.Getenv("PG_HOST"),
+			Port:     os.Getenv("PG_PORT"),
+			Username: os.Getenv("PG_USER"),
+			Password: os.Getenv("PG_PASSWORD"),
+			DBName:   os.Getenv("PG_DATABASE_NAME"),
+		}
+	})
+	logger.Info("Config", "Config init")
+	return &config
+}
 
 const (
 	host     = "localhost"
