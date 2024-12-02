70f4652c5323e97f1239282ed9cee775d3b0c715
MentalMentos <rkorolev026@gmail.com>
2024-12-02 14:43:09 +0300

    add libr

--- migrations/keepalivedb.go
+++ migrations/keepalivedb.go
@@ -6,6 +6,7 @@ import (
 	"github.com/MentalMentos/techTaskMetr.git/pkg/helpers"
 	"github.com/MentalMentos/techTaskMetr.git/pkg/logger"
 	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
