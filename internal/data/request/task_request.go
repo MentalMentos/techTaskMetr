01308d7ff08b89ca1ff0d836e8e63c2143651274
MentalMentos <rkorolev026@gmail.com>
2025-01-13 14:47:42 +0300

    add status in request

--- internal/data/request/task_request.go
+++ internal/data/request/task_request.go
@@ -3,12 +3,14 @@ package request
 type CreateTaskRequest struct {
 	Title       string `json:"title" validate:"required"`       // Название задачи (обязательно)
 	Description string `json:"description" validate:"required"` // Описание задачи (обязательно)
+	Status      string `json:"status" validate:"required"`      //bool
 }
 
 type UpdateTaskRequest struct {
 	Id          string `json:"id" binding:"required"`
 	Title       string `json:"title" binding:"required"`
 	Description string `json:"description" binding:"required"`
+	Status      string `json:"status" binding:"required"`
 }
 
 type DeleteTaskRequest struct {
