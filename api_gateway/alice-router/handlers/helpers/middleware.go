b6e1410e74b094750574a31f83f88755b8d51530
MentalMentos <rkorolev026@gmail.com>
2025-01-16 01:28:43 +0300

    add requests for alice1

--- api_gateway/alice-router/handlers/helpers/middleware.go
+++ api_gateway/alice-router/handlers/helpers/middleware.go
@@ -23,7 +23,6 @@ func AuthMiddleware(c *gin.Context) {
 		var registerReq handlers.RegisterRequest
 		registerReq.Email = loginReq.Email
 		registerReq.Password = loginReq.Password
-		registerReq.Name = "Пользователь" // Или запросите имя отдельно
 
 		authResp, err = TryRegister(registerReq)
 		if err != nil {
